78299d400327020e1781d3b052841db85fd8d6c9
Andrew Chelladurai <TheUnknownAndrew@Gmail.Com>
2019-12-31 00:54:09 +0530

    Restart using BottomAppBar and updates package / library dependencies

---
+++ simplebible/src/main/java/com/andrewchelladurai/app/simplebible/SimpleBible.java
@@ -0,0 +1,18 @@
+package com.andrewchelladurai.app.simplebible;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.andrewchelladurai.simplebible.R;
+
+public class SimpleBible
+    extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_simple_bible);
+  }
+
+}
