f8cb9494047396597e3c5df6ddcf2789d773ad44
Andrew Chelladurai <TheUnknownAndrew@Gmail.Com>
2020-01-12 15:43:37 +0530

    Create blank ScreenFragments, use a NavHostFragment + BottomAppBar

--- simplebible/src/main/java/com/andrewchelladurai/app/simplebible/SimpleBible.java
+++ simplebible/src/main/java/com/andrewchelladurai/app/simplebible/SimpleBible.java
@@ -4,10 +4,28 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.andrewchelladurai.app.simplebible.ui.ScreenBookDetails;
+import com.andrewchelladurai.app.simplebible.ui.ScreenBookList;
+import com.andrewchelladurai.app.simplebible.ui.ScreenBookmarkDetails;
+import com.andrewchelladurai.app.simplebible.ui.ScreenBookmarkList;
+import com.andrewchelladurai.app.simplebible.ui.ScreenChapter;
+import com.andrewchelladurai.app.simplebible.ui.ScreenHome;
+import com.andrewchelladurai.app.simplebible.ui.ScreenMessage;
+import com.andrewchelladurai.app.simplebible.ui.ScreenSearch;
+import com.andrewchelladurai.app.simplebible.ui.ScreenSettings;
 import com.andrewchelladurai.simplebible.R;
 
 public class SimpleBible
-    extends AppCompatActivity {
+    extends AppCompatActivity
+    implements ScreenBookDetails.InteractionListener,
+               ScreenBookList.InteractionListener,
+               ScreenBookmarkDetails.InteractionListener,
+               ScreenBookmarkList.InteractionListener,
+               ScreenChapter.InteractionListener,
+               ScreenHome.InteractionListener,
+               ScreenMessage.InteractionListener,
+               ScreenSearch.InteractionListener,
+               ScreenSettings.InteractionListener {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
---
+++ simplebible/src/main/java/com/andrewchelladurai/app/simplebible/ui/ScreenMessage.java
@@ -0,0 +1,49 @@
+package com.andrewchelladurai.app.simplebible.ui;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.andrewchelladurai.simplebible.R;
+
+public class ScreenMessage
+    extends Fragment {
+
+  private InteractionListener listener;
+
+  public ScreenMessage() {
+    // Required empty public constructor
+  }
+
+  @Override
+  public void onAttach(@NonNull Context context) {
+    super.onAttach(context);
+    if (context instanceof InteractionListener) {
+      listener = (InteractionListener) context;
+    } else {
+      throw new RuntimeException(context.toString() + " must implement InteractionListener");
+    }
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    return inflater.inflate(R.layout.screen_message, container, false);
+  }
+
+  @Override
+  public void onDetach() {
+    super.onDetach();
+    listener = null;
+  }
+
+  public interface InteractionListener {
+
+  }
+
+}
