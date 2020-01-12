e943289eb653a0b5931bcd957641e50d62b320be
Andrew Chelladurai <TheUnknownAndrew@Gmail.Com>
2020-01-12 20:33:18 +0530

    Create notification channels method & expose navigation methods through Ops Interface

--- simplebible/src/main/java/com/andrewchelladurai/app/simplebible/SimpleBible.java
+++ simplebible/src/main/java/com/andrewchelladurai/app/simplebible/SimpleBible.java
@@ -1,7 +1,13 @@
 package com.andrewchelladurai.app.simplebible;
 
+import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -25,6 +31,7 @@ public class SimpleBible
 
     final BottomAppBar bar = findViewById(R.id.activity_bottom_app_bar);
     bar.setOnMenuItemClickListener(item -> {
+      hideKeyboard();
       switch (item.getItemId()) {
         case R.id.activity_bottom_app_bar_menu_home:
           return showScreenHome();
@@ -43,36 +50,76 @@ public class SimpleBible
 
     navController = Navigation.findNavController(findViewById(R.id.activity_nav_host_fragment));
 
+    if (savedInstanceState == null) {
+      // Need this only once when the application launches
+      createNotificationChannel();
+    }
+
   }
 
-  private boolean showScreenHome() {
+  public boolean showScreenHome() {
     Log.d(TAG, "showScreenHome:");
     navController.navigate(R.id.show_screenHome);
     return true;
   }
 
-  private boolean showScreenBooksList() {
+  private void createNotificationChannel() {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+      Log.d(TAG, "createNotificationChannel: Skipped coz feature not present in Android "
+                 + "versions lower than Oreo / Android 8.0 (API level 26)");
+      return;
+    }
+
+    NotificationChannel notificationChannel = new NotificationChannel(
+        getPackageName(),
+        getString(R.string.app_notif_channel_name),
+        NotificationManager.IMPORTANCE_HIGH);
+    notificationChannel.setDescription(getString(R.string.app_notif_channel_desc));
+
+    final NotificationManager systemService = getSystemService(NotificationManager.class);
+    if (systemService != null) {
+      systemService.createNotificationChannel(notificationChannel);
+      Log.d(TAG, "createNotificationChannel: created");
+    } else {
+      Log.e(TAG, "createNotificationChannel: failed [Null systemService received]");
+    }
+  }
+
+  public boolean showScreenBooksList() {
     Log.d(TAG, "showScreenBooksList:");
     navController.navigate(R.id.show_screenBookList);
     return true;
   }
 
-  private boolean showScreenSearch() {
+  public boolean showScreenSearch() {
     Log.d(TAG, "showScreenSearch:");
     navController.navigate(R.id.show_screenSearch);
     return true;
   }
 
-  private boolean showScreenBookmarksList() {
+  public boolean showScreenBookmarksList() {
     Log.d(TAG, "showScreenBookmarksList:");
     navController.navigate(R.id.show_screenBookmarkList);
     return true;
   }
 
-  private boolean showScreenSettings() {
+  public boolean showScreenSettings() {
     Log.d(TAG, "showScreenSettings:");
     navController.navigate(R.id.show_screenSettings);
     return true;
+  }
+
+  public void hideKeyboard() {
+    final InputMethodManager inputMethodManager =
+        (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+
+    //Find the currently focused view, so we can grab the correct window token from it.
+    //If no view currently has focus, create a new one, just so we can grab a window token from it
+    final View view = (getCurrentFocus() != null) ? getCurrentFocus() : new View(this);
+
+    if (inputMethodManager != null) {
+      inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
   }
 
 }
--- simplebible/src/main/java/com/andrewchelladurai/app/simplebible/ops/SimpleBibleOps.java
+++ simplebible/src/main/java/com/andrewchelladurai/app/simplebible/ops/SimpleBibleOps.java
@@ -2,4 +2,16 @@ package com.andrewchelladurai.app.simplebible.ops;
 
 public interface SimpleBibleOps {
 
+  boolean showScreenHome();
+
+  boolean showScreenBooksList();
+
+  boolean showScreenSearch();
+
+  boolean showScreenBookmarksList();
+
+  boolean showScreenSettings();
+
+  void hideKeyboard();
+
 }
